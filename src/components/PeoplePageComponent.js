e98a9e79260b7790c009bbf3fee4a1108fbcc08f
GlowingEgg <qhinman@gmail.com>
2018-11-20 13:01:50 -0800

    Makes a carousel in the PeoplePage.

--- src/components/PeoplePageComponent.js
+++ src/components/PeoplePageComponent.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import Slider from "react-slick";
 import './PeoplePageComponent.css';
 
-const PeoplePageComponent = props => (
-  <section className = "PeoplePage">
-  <div className = "PeoplePageTextContent">
-    <p className = "PeoplePageTitle">Who We Are</p>
-    <p className = "PeoplePageText">We're a verteran team of startup designers and engineers</p>
-  </div>
-  </section>
-)
+const PeoplePageComponent = props => {
+  var settings = {
+      autoplay: true,
+      autoplaySpeed: 5000,
+      arrows: true,
+      infinite: true,
+      speed: 1500,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: '200px'
+    };
+  return(
+    <section className = "PeoplePage">
+      <div className = "PeoplePageTextContent">
+        <p className = "PeoplePageTitle">Who We Are</p>
+        <p className = "PeoplePageText">We're a verteran team of startup designers and engineers</p>
+      </div>
+      <div className = "PeoplePageCarouselContainer">
+        <Slider {...settings}>
+          <div><div className = "PeoplePageCarouselItem"></div></div>
+          <div><div className = "PeoplePageCarouselItem"></div></div>
+          <div><div className = "PeoplePageCarouselItem"></div></div>
+          <div><div className = "PeoplePageCarouselItem"></div></div>
+          <div><div className = "PeoplePageCarouselItem"></div></div>
+        </Slider>
+      </div>
+    </section>
+  );
+}
 
 export default PeoplePageComponent;
