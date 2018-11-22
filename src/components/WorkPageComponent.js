7194dce1973e4be3ba61522569735d8d0021b1be
GlowingEgg <qhinman@gmail.com>
2018-11-21 23:00:41 -0800

    Fixes some alignment in the middle sections.

--- src/components/WorkPageComponent.js
+++ src/components/WorkPageComponent.js
@@ -7,11 +7,26 @@ const WorkPageComponent = props => {
   // Slider settings
   var settings = {
       autoplay: true,
-      autoplaySpeed: 5000,
+      autoplaySpeed: 3000,
       infinite: true,
       speed: 1500,
       slidesToShow: 4,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      pauseOnHover: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
     };
 
   return (
