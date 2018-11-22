df0b4edb095572a759c114c8ca47a259f7a071c2
GlowingEgg <qhinman@gmail.com>
2018-11-21 16:13:28 -0800

    Mobilizes a bit.

--- src/components/HomePageComponent.js
+++ src/components/HomePageComponent.js
@@ -7,7 +7,6 @@ const HomePageComponent = (props) => (
       <img className = "SSALogo" src = "ssa-logo.png" alt = "SSA Logo"></img>
       <p className = "SSATitle">Sierra Space Agency</p>
     </header>
-    <img className = "SSAImage" src = "faded-sierra-mountains.png"/>
   </section>
 )
 
