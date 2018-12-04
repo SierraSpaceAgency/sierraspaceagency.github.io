4f1f3ce4f8236010a9de7d1f42e64863cd7e13e8
Brian Cox <therealbriancox@gmail.com>
2018-12-04 15:58:55 -0800

    Updates

---
+++ precache-manifest.8c3c2bb7140e69a5cd732685240bcc42.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecc1d5049436185b805eeee09e199ebe",
+    "url": "/ssa-front/static/media/faded-sierra-mountains.ecc1d504.png"
+  },
+  {
+    "revision": "eb9ec08a175971d6d4bcb36bff5a6b46",
+    "url": "/ssa-front/static/media/HKGrotesk-Regular.eb9ec08a.otf"
+  },
+  {
+    "revision": "1fcfefce27bbf40f4afbace8277a8ed3",
+    "url": "/ssa-front/static/media/HKGrotesk-Medium.1fcfefce.otf"
+  },
+  {
+    "revision": "646f8450b5eb22378a905772db592ab7",
+    "url": "/ssa-front/static/media/HKGrotesk-Light.646f8450.otf"
+  },
+  {
+    "revision": "8d97d9571f6641185a2e",
+    "url": "/ssa-front/static/js/runtime~main.8d97d957.js"
+  },
+  {
+    "revision": "db5b9317d6a618169438",
+    "url": "/ssa-front/static/js/main.db5b9317.chunk.js"
+  },
+  {
+    "revision": "bbf08e84ce4443ed557e",
+    "url": "/ssa-front/static/js/1.bbf08e84.chunk.js"
+  },
+  {
+    "revision": "db5b9317d6a618169438",
+    "url": "/ssa-front/static/css/main.f3048f81.chunk.css"
+  },
+  {
+    "revision": "25a79f7d9abd05b5891d30ab0b208634",
+    "url": "/ssa-front/index.html"
+  }
+];
