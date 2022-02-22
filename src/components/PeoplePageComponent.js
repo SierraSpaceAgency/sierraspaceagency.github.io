71e462d72eb66642867995afc7b3413c7f45211e
Brian Cox <brian@therealbriancox.com>
2022-02-22 15:05:04 -0800

    Adds new people

--- src/components/PeoplePageComponent.js
+++ src/components/PeoplePageComponent.js
@@ -9,40 +9,50 @@ const PeoplePageComponent = props => (
     </div>
     <ul className = "PeopleList">
       <li className = "Person">
-        <a className = "PersonLink" href = "https://andrewferrarone.com">
-          <img className = "PersonImage" src = "Andy.png" alt = ""/>
-          <div className = "Label PersonName">Andy</div>
-          <div className = "Label PersonTitle">DESIGN</div>
-        </a>
-      </li>
-      <li className = "Person">
         <a className = "PersonLink" href = "http://briancox.co">
           <img className = "PersonImage" src = "Brian.png" alt = ""/>
           <div className = "Label PersonName">Brian</div>
-          <div className = "Label PersonTitle">ARCHITECT</div>
+          <div className = "Label PersonTitle">Product</div>
+        </a>
+      </li>
+      <li className = "Person">
+        <a className = "PersonLink" href = "https://andrewferrarone.com">
+          <img className = "PersonImage" src = "Andy.png" alt = ""/>
+          <div className = "Label PersonName">Andy</div>
+          <div className = "Label PersonTitle">Design</div>
         </a>
       </li>
       <li className = "Person">
         <a className = "PersonLink" href = "https://connerhinman.com">
           <img className = "PersonImage" src = "Conner.png" alt = ""/>
           <div className = "Label PersonName">Conner</div>
-          <div className = "Label PersonTitle">ENGINEERING</div>
+          <div className = "Label PersonTitle">Engineering</div>
         </a>
       </li>
       <li className = "Person">
         <img className = "PersonImage" src = "Grace.png" alt = ""/>
         <div className = "Label PersonName">Grace</div>
-        <div className = "Label PersonTitle">ENGINEERING</div>
+        <div className = "Label PersonTitle">Engineering</div>
         </li>
       <li className = "Person">
         <img className = "PersonImage" src = "Kevin.png" alt = ""/>
         <div className = "Label PersonName">Kevin</div>
-        <div className = "Label PersonTitle">ENGINEERING</div>
+        <div className = "Label PersonTitle">Engineering</div>
       </li>
       <li className = "Person">
         <img className = "PersonImage" src = "Sally.png" alt = ""/>
         <div className = "Label PersonName">Sally</div>
-        <div className = "Label PersonTitle">ENGINEERING</div>
+        <div className = "Label PersonTitle">Engineering</div>
+      </li>
+      <li className = "Person">
+        <img className = "PersonImage" src = "Rosie.png" alt = ""/>
+        <div className = "Label PersonName">Rosie</div>
+        <div className = "Label PersonTitle">Engineering</div>
+      </li>
+      <li className = "Person">
+        <img className = "PersonImage" src = "Ruiz.png" alt = ""/>
+        <div className = "Label PersonName">Ruiz</div>
+        <div className = "Label PersonTitle">Engineering</div>
       </li>
     </ul>
   </section>
