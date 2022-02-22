40c404957fd332502259a23667f7b269dfc96315
Brian Cox <brian@therealbriancox.com>
2022-02-22 15:14:20 -0800

    Updates

--- precache-manifest.f2ada9a350f161ae5d603b0f688196e4.js
+++ precache-manifest.f2ada9a350f161ae5d603b0f688196e4.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "bc360bd68c50ac3dc772",
-    "url": "/static/css/main.1c81edf2.chunk.css"
+    "revision": "149920ad1b1ae303d907",
+    "url": "/static/css/main.3c3f88f4.chunk.css"
   },
   {
-    "revision": "bc360bd68c50ac3dc772",
-    "url": "/static/js/main.bc360bd6.chunk.js"
+    "revision": "149920ad1b1ae303d907",
+    "url": "/static/js/main.149920ad.chunk.js"
   },
   {
-    "revision": "e004b97a1e192ab200fd",
-    "url": "/static/js/1.e004b97a.chunk.js"
+    "revision": "63560536718b5ca91669",
+    "url": "/static/js/1.63560536.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
@@ -20,19 +20,19 @@ self.__precacheManifest = [
     "url": "/static/media/HKGrotesk-Light.646f8450.otf"
   },
   {
-    "revision": "1fcfefce27bbf40f4afbace8277a8ed3",
-    "url": "/static/media/HKGrotesk-Medium.1fcfefce.otf"
-  },
-  {
     "revision": "eb9ec08a175971d6d4bcb36bff5a6b46",
     "url": "/static/media/HKGrotesk-Regular.eb9ec08a.otf"
+  },
+  {
+    "revision": "1fcfefce27bbf40f4afbace8277a8ed3",
+    "url": "/static/media/HKGrotesk-Medium.1fcfefce.otf"
   },
   {
     "revision": "ecc1d5049436185b805eeee09e199ebe",
     "url": "/static/media/faded-sierra-mountains.ecc1d504.png"
   },
   {
-    "revision": "9ef153d374fe8c2e89e5e7a096b8b17f",
+    "revision": "d4bf7fc26522d613cdf0478f5129275b",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d76f10056bb951f784f4670ffeb1a385.js"
+  "/precache-manifest.f2ada9a350f161ae5d603b0f688196e4.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
